3883543000ea6f1b742f9202b3182cea48da5925
Giovix92 <ggualtierone@gmail.com>
2019-10-17 01:25:05 +0200

    1.4 changes
    1.4:
    Implemented full server build function
    Implemented poweroff function with failsafe for laptop mode
    Fixed help part
    Reworked tgerr function, as well as servercmd function
    Added check before starting the party, to check whether CI should use servercmd or not

--- bot.sh
+++ bot.sh
@@ -2,7 +2,7 @@
 # SET MAIN VARIABLES
 TELEGRAM_TOKEN=$SECRET_TOKEN
 TELEGRAM_CHAT=$CHAT_ID
-TELEGRAM="/home/giovix92/CI/tg/tgram"
+TELEGRAM="/home/giovix92/CI/tg/telegram"
 IP=$SERVER_IP
 USERNAME=$SERVER_USERNAME
 PASSWORD=$SERVER_PASSWORD
@@ -18,8 +18,15 @@ tgsay() {
 }
 
 tgerr () {
-	$TELEGRAM -t $TELEGRAM_TOKEN -c $TELEGRAM_CHAT -f "$2" "$1"
-  exit 1
+	if [ "$TAKELOGS" == "true" ]; then
+	  $TELEGRAM -t $TELEGRAM_TOKEN -c $TELEGRAM_CHAT -f "$2" "$1"
+	elif [ "$TAKELOGS" == "false" ]; then
+	  $TELEGRAM -t $TELEGRAM_TOKEN -c $TELEGRAM_CHAT "$1"
+	fi
+	if [ "$POWEROFF" == "true" ]; then
+		poweroff --force
+	fi
+    exit
 }
 
 syncsauce() {
@@ -45,9 +52,9 @@ echoo() {
     )"
 }
 
-server() {
+servercmd() {
 	if [ "$SERVER" == "true" ]; then
-	  serveropt=$(sshpass -p "$PASSWORD" ssh -o StrictHostKeyChecking=no $USERNAME@$IP)
+	  sshpass -p "$PASSWORD" ssh -o StrictHostKeyChecking=no $USERNAME@$IP "$1"
     fi
 }
 
@@ -66,12 +73,12 @@ elif [ "$1" == "" ]; then
   echo "Please, provide an option, or type help."
   exit
 elif [ "$1" == "help" ]; then
-  echo "Giovix92 CI Bot v1.3"
-  echo "Command usage: bot.sh device romtype [nosync] [clean] [takelogs]"
+  echo "Giovix92 CI Bot v1.4"
+  echo "Command usage: bot.sh device romtype [nosync] [clean] [takelogs] [server] [poweroff]"
   echo "Goodbye!"
   exit
 elif [ "$1" == "changelog" ]; then
-  echo "Giovix92 CI Bot 1.3"
+  echo "Giovix92 CI Bot 1.4"
   cat ./changelog.txt
   echo "Goodbye!"
   exit
@@ -99,28 +106,28 @@ elif [ "$2" == "revenge9" ]; then
   ANDROIDVER="Q"
   TYPE=ROM
 fi
-if [ "$3" == "nosync" ] || [ "$4" == "nosync" ] || [ "$5" == "nosync" ] || [ "$6" == "nosync" ]; then
+if [ "$3" == "nosync" ] || [ "$4" == "nosync" ] || [ "$5" == "nosync" ] || [ "$6" == "nosync" ] || [ "$7" == "nosync" ]; then
   NOSYNC=true
   message="Nosync option added! Skipping syncing."
 else
   NOSYNC=false
   message="No "nosync" option provided! Syncing."
 fi
-if [ "$3" == "clean" ] || [ "$4" == "clean" ] || [ "$5" == "clean" ] || [ "$6" == "clean" ]; then
+if [ "$3" == "clean" ] || [ "$4" == "clean" ] || [ "$5" == "clean" ] || [ "$6" == "clean" ] || [ "$7" == "clean" ]; then
   CLEAN=true
   messagetwo="Clean option provided! Making a clean build."
 else
   CLEAN=false
   messagetwo="No "clean" option provided! Making a dirty build."
 fi
-if [ "$3" == "takelogs" ] || [ "$4" == "takelogs" ] || [ "$5" == "takelogs" ] || [ "$6" == "takelogs" ]; then
+if [ "$3" == "takelogs" ] || [ "$4" == "takelogs" ] || [ "$5" == "takelogs" ] || [ "$6" == "takelogs" ] || [ "$7" == "takelogs" ]; then
   TAKELOGS=true
   messagethree="Takelogs option provided! Running in logging mode."
 else
   TAKELOGS=false
   messagethree="No "takelogs" option provided! Running in silent mode."
 fi
-if [ "$3" == "server" ] || [ "$4" == "server" ] || [ "$5" == "server" ] || [ "$6" == "server" ]; then
+if [ "$3" == "server" ] || [ "$4" == "server" ] || [ "$5" == "server" ] || [ "$6" == "server" ] || [ "$7" == "server" ]; then
   SERVER=true
   messagefour="Server option provided! Using server as build machine."
 else
@@ -128,26 +135,44 @@ else
   messagefour="No "server" option provided. Using laptop as build machine."
 fi
 
+# INITIAL VAR EXPORT
 export WORKINGDIR BUILDTYPE TYPE CLEAN NOSYNC TAKELOGS ANDROIDVER VARIANT SERVER
 export message messagetwo messagethree messagefour
+
+if [ "$3" == "poweroff" ] || [ "$4" == "poweroff" ] || [ "$5" == "poweroff" ] || [ "$6" == "poweroff" ] || [ "$7" == "poweroff" ]; then
+  if [ "$SERVER" == "true" ]; then
+  	POWEROFF=false
+  	messagefive="WARNING: Poweroff mode not available when in server build mode! Ignoring."
+  else
+    POWEROFF=true
+    messagefive="Poweroff option provided! Turning off laptop after work!"
+  fi
+else
+  POWEROFF=false
+  messagefive="No "poweroff" option provided. Laptop will stay on."
+fi
 
 date=$(date +%Y%m%d)
 starttime=$(date +%H%M)
 jobs=$(nproc --all)
 
-tgsay "$BUILDTYPE $ANDROIDVER build rolled at $date $starttime CEST!" "Device: $DEVICE, type: $TYPE" "Additional infos:" "$message" "$messagetwo" "$messagethree" "$messagefour"
+# FINAL VAR EXPORT
+export POWEROFF messagefive date starttime jobs
+
+tgsay "$BUILDTYPE $ANDROIDVER build rolled at $date $starttime CEST!" "Device: $DEVICE, type: $TYPE" "Additional infos:" "$message" "$messagetwo" "$messagethree" "$messagefour" "$messagefive"
 if [ "$SERVER" == "true" ]; then
   if [ "$2" == "revenge10" ]; then
-    WORKINGDIR="/media/giovix92/HDD/RevengeOS10"
+    WORKINGDIR="ros_10"
   elif [ "$2" == "revenge9" ]; then
-    WORKINGDIR="/media/giovix92/HDD/RevengeOS"
+    WORKINGDIR=
   elif [ "$2" == "twrp" ]; then
     WORKINGDIR=
     echo "TWRP building on server is currently not possible. Aborting."
     exit
   fi
+elif [ "$SERVER" == "false" ]; then
+  cd $WORKINGDIR
 fi
-cd $WORKINGDIR
 
 if [ "$NOSYNC" == "false" ]; then
   syncsauce
@@ -166,29 +191,62 @@ else
   echo "Vars are set, continuing"
 fi
 
-# START THE PARTY
-. build/envsetup.sh
+### START THE PARTY, NO SERVER ###
+if [ "$SERVER" == "false" ]; then
+  . build/envsetup.sh
 
-# CHECK FOR LOGS
-if [ -f "./log*.txt" ]; then
-	rm log*.txt
-fi
-
-# CHECK CLEAN VAR
-if [ "$CLEAN" == true ]; then
-  "make clean" || tgsay "ERROR: Cleaning out/ terminated prematurely."
-  if [ "$TYPE" == "ROM" ]; then
-    "make clobber" || tgsay "ERROR: Cleaning out/ terminated prematurely."
+  # CHECK FOR LOGS
+  if [ -f "log*.txt" ]; then
+    rm log*.txt
   fi
-  ". build/envsetup.sh"
-fi
 
-# LUNCH PART
-"lunch "$(echo $WORKNAME)_$(echo $DEVICE)-$(echo $VARIANT)"" 2>&1 | tee "loglunch-$BUILDTYPE-$ANDROIDVER-$date-$starttime.txt" || tgerr "ERROR: $BUILDTYPE $ANDROIDVER lunch failed! Check log." "loglunch-$BUILDTYPE-$ANDROIDVER-$date-$starttime.txt"
-if [ "$TYPE" == "ROM" ]; then
-  "brunch $DEVICE -j$jobs" 2>&1 | tee "logbuild-$BUILDTYPE-$ANDROIDVER-$date-$starttime.txt" || tgerr "ERROR: $BUILDTYPE $ANDROIDVER lunch failed! Check log." "logbuild-$BUILDTYPE-$ANDROIDVER-$date-$starttime.txt"
-else
-  "lunch "$(echo $WORKNAME)_$(echo $DEVICE)-$(echo $VARIANT)" && make O=out recoveryimage" 2>&1 | tee "logbuild-$BUILDTYPE-$ANDROIDVER-$date-$starttime.txt" || tgerr "ERROR: $BUILDTYPE $ANDROIDVER lunch failed! Check log." "logbuild-$BUILDTYPE-$ANDROIDVER-$date-$starttime.txt"
+  # CHECK CLEAN VAR
+  if [ "$CLEAN" == true ]; then
+    make clean || tgsay "ERROR: Cleaning out/ terminated prematurely."
+    if [ "$TYPE" == "ROM" ]; then
+      make clobber || tgsay "ERROR: Cleaning out/ terminated prematurely."
+    fi
+    . build/envsetup.sh
+  fi
+
+  # LUNCH PART
+  lunch "$(echo $WORKNAME)_$(echo $DEVICE)-$(echo $VARIANT)" 2>&1 | tee "loglunch-$BUILDTYPE-$ANDROIDVER-$date-$starttime.txt" || tgerr "ERROR: $BUILDTYPE $ANDROIDVER lunch failed! Check log." "loglunch-$BUILDTYPE-$ANDROIDVER-$date-$starttime.txt"
+  if [ "$TYPE" == "ROM" ]; then
+    brunch $DEVICE -j$jobs 2>&1 | tee "logbuild-$BUILDTYPE-$ANDROIDVER-$date-$starttime.txt" || tgerr "ERROR: $BUILDTYPE $ANDROIDVER lunch failed! Check log." "logbuild-$BUILDTYPE-$ANDROIDVER-$date-$starttime.txt"
+  else
+    lunch "$(echo $WORKNAME)_$(echo $DEVICE)-$(echo $VARIANT)" && make O=out recoveryimage 2>&1 | tee "logbuild-$BUILDTYPE-$ANDROIDVER-$date-$starttime.txt" || tgerr "ERROR: $BUILDTYPE $ANDROIDVER lunch failed! Check log." "logbuild-$BUILDTYPE-$ANDROIDVER-$date-$starttime.txt"
+  fi
+  tgsay "$BUILDTYPE $ANDROIDVER build finished successfully!"
+  exit
+
+### START THE PARTY, USING SERVER ###
+elif [ "$SERVER" == "true" ]; then
+	servercmd ". build/envsetup.sh"
+
+  # CHECK FOR LOGS
+  if [ -f "log*.txt" ]; then
+    servercmd "rm log*.txt"
+  fi
+
+  # CHECK CLEAN VAR
+  if [ "$CLEAN" == true ]; then
+    servercmd "make clean" || tgsay "ERROR: Cleaning out/ terminated prematurely."
+    if [ "$TYPE" == "ROM" ]; then
+      servercmd "make clobber" || tgsay "ERROR: Cleaning out/ terminated prematurely."
+    fi
+    servercmd ". build/envsetup.sh"
+  fi
+
+  # LUNCH PART
+  servercmd "lunch "$(echo $WORKNAME)_$(echo $DEVICE)-$(echo $VARIANT)"" 2>&1 | tee "loglunch-$BUILDTYPE-$ANDROIDVER-$date-$starttime.txt" || tgerr "ERROR: $BUILDTYPE $ANDROIDVER lunch failed! Check log." "loglunch-$BUILDTYPE-$ANDROIDVER-$date-$starttime.txt"
+  if [ "$TYPE" == "ROM" ]; then
+    servercmd "brunch $DEVICE -j$jobs 2>&1" | tee "logbuild-$BUILDTYPE-$ANDROIDVER-$date-$starttime.txt" || tgerr "ERROR: $BUILDTYPE $ANDROIDVER lunch failed! Check log." "logbuild-$BUILDTYPE-$ANDROIDVER-$date-$starttime.txt"
+  else
+    servercmd "lunch "$(echo $WORKNAME)_$(echo $DEVICE)-$(echo $VARIANT)" && make O=out recoveryimage" 2>&1 | tee "logbuild-$BUILDTYPE-$ANDROIDVER-$date-$starttime.txt" || tgerr "ERROR: $BUILDTYPE $ANDROIDVER lunch failed! Check log." "logbuild-$BUILDTYPE-$ANDROIDVER-$date-$starttime.txt"
+  fi
+  tgsay "$BUILDTYPE $ANDROIDVER build finished successfully!"
+  if [ "$POWEROFF" == "true" ]; then
+		poweroff --force
+	fi
+  exit
 fi
-tgsay "$BUILDTYPE $ANDROIDVER build finished successfully!"
-exit
